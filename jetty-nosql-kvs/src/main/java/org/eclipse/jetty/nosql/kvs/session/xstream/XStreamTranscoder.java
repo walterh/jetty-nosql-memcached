1dd17bc3dbfb241d99db52b2b172f0dbf965af17
Walter <walter@castar.com>
2016-10-25 21:34:06 -0700

    update to use serialization instead of xstream (reflection).

--- jetty-nosql-kvs/src/main/java/org/eclipse/jetty/nosql/kvs/session/xstream/XStreamTranscoder.java
+++ jetty-nosql-kvs/src/main/java/org/eclipse/jetty/nosql/kvs/session/xstream/XStreamTranscoder.java
@@ -1,6 +1,8 @@
 package org.eclipse.jetty.nosql.kvs.session.xstream;
 
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.io.StringWriter;
 
 import org.apache.commons.lang3.SerializationUtils;
 import org.eclipse.jetty.nosql.kvs.session.ISerializationTranscoder;
@@ -8,9 +10,13 @@ import org.eclipse.jetty.nosql.kvs.session.TranscoderException;
 
 import com.thoughtworks.xstream.XStream;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class XStreamTranscoder implements ISerializationTranscoder {
+    private final static boolean SERIALIZABILITY_CHECK = true;
     private XStream xstream = null;
-    private ClassLoader classLoader = null;
+    //private ClassLoader classLoader = null;
 
     public XStreamTranscoder() {
         this(Thread.currentThread().getContextClassLoader());
@@ -21,10 +27,23 @@ public class XStreamTranscoder implements ISerializationTranscoder {
         // FIXME: using SessionManager's `getContext().getClassLoader()` causes `com.thoughtworks.xstream.mapper.CannotResolveClassException`
         //        on deserializing `org.eclipse.jetty.nosql.kvs.session.xstream.XStreamSession`
         //  xstream.setClassLoader(cl);
-        classLoader = cl;
+        //classLoader = cl;
     }
 
     public byte[] encode(Object obj) throws TranscoderException {
+        if (SERIALIZABILITY_CHECK && obj instanceof Serializable) {
+            // unsafe cast, but we really should not be sending anything that isn't serializable
+            return SerializationUtils.serialize((Serializable) obj);
+        } else {
+            byte[] raw = null;
+            try {
+                raw = xstream.toXML(obj).getBytes("UTF-8");
+            } catch (Exception error) {
+                throw (new TranscoderException(error));
+            }
+            
+            return raw;
+        }
         /*
         final long t1 = System.currentTimeMillis();
         byte[] raw = null;
@@ -44,13 +63,31 @@ public class XStreamTranscoder implements ISerializationTranscoder {
         return serializedBytes;
         */
         
-        // unsafe cast, but we really should not be sending anything that isn't serializable
-        return SerializationUtils.serialize((Serializable) obj);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T decode(byte[] raw, Class<T> klass) throws TranscoderException {
-        return SerializationUtils.deserialize(raw);
+        // we could check the interfaces, but requires reflection...do a simple xstream xml test...
+        T obj = null;
+        
+        try {
+            if (raw[0] == '<') {
+                obj = (T) xstream.fromXML(new String(raw, "UTF-8"));
+            } else {
+                obj = SerializationUtils.deserialize(raw);
+            }
+        } catch (Exception e) {
+            //throw (new TranscoderException(e));
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw, true);
+            e.printStackTrace(pw);
+            pw.flush();
+            sw.flush();
+            
+            log.error(sw.toString());
+        }
+        
+        return obj;
         
         /*
         T obj = null;
